a3fc53e4b4c1b2cd1ce746ba96377266e952cce4
Shaheryaar Kamal <shaheryaarkamal@gmail.com>
2021-03-30 14:34:43 +0500

    added show user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,12 @@ class UsersController < ApplicationController
     def index
     end
 
+    def show
+        @user = User.find(params[:id])
+        @articles = @user.articles
+      
+    end
+
     def new
         @user = User.new
     end
